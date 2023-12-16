1ab99e746edac4e15c01cb4d4cca2117dc69b4e6
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-12-16 17:42:19 +0700

    tests: fix SuiteName-TestName naming

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -4,14 +4,14 @@
 // explicit template class instantiation for the needs of code coverage
 template class s21::vector<int>;
 
-TEST(constructor, default_) {
+TEST(VectorCtorDefault, Size) {
   const int sz = 0;
   s21::vector<int> v;
   // EXPECT_TRUE(v.empty());
   EXPECT_EQ(sz, v.size());
 }
 
-TEST(constructor, parametrized) {
+TEST(VectorCtorParametrized, Size) {
   const int sz = 5;
   s21::vector<int> v(sz);
   EXPECT_EQ(sz, v.size());
