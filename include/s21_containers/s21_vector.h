0edba9460a00e502b42db4daa8804cf398b2dd5c
Мария Нардюжева <masam@MSI>
2024-01-17 10:30:10 +0700

    17/01

--- include/s21_containers/s21_vector.h
+++ include/s21_containers/s21_vector.h
@@ -1,100 +1,299 @@
 #ifndef S21_VECTOR_H_
 #define S21_VECTOR_H_
 
-#include <cstddef>
+#include <algorithm>
 #include <initializer_list>
+#include <limits>
 #include <utility>
 
 namespace s21 {
 
-// Test vector class with some basic example operations and concepts
-template <class T>
+template <typename T>
 class vector {
- private:
-  size_t m_size;
-  size_t m_capacity;
-  T *arr;
-
  public:
   using value_type = T;
   using reference = T &;
   using const_reference = const T &;
   using iterator = T *;
   using const_iterator = const T *;
-  using size_type = size_t;
+  using size_type = std::size_t;
+
+  // Member functions
+ public:
+  vector() : size_(0), capacity_(0), data_(nullptr) {}
+
+  explicit vector(size_type n) {
+    size_ = n;
+    capacity_ = n;
+    data_ = nullptr;
+    if (n > 0) {
+      data_ = new value_type[capacity_];
+    }
+  }
+
+  explicit vector(std::initializer_list<value_type> const &items)
+      : size_{items.size()},
+        capacity_(items.size()),
+        data_{new value_type[capacity_]} {
+    std::copy(items.begin(), items.end(), data_);
+  }
+
+  vector(const vector &v)
+      : size_{v.size_},
+        capacity_{v.capacity_},
+        data_{new value_type[capacity_]} {
+    if (data_ != nullptr) {
+      std::copy(v.data_, v.data_ + v.size_, data_);
+    }
+  }
+
+  vector(vector &&v) noexcept
+      : size_{std::exchange(v.size_, 0)},
+        capacity_{std::exchange(v.capacity_, 0)},
+        data_{std::exchange(v.data_, nullptr)} {}
+
+  ~vector() { delete[] data_; }
+
+  vector &operator=(vector &&v) noexcept {
+    if (this != &v) {
+      delete[] data_;
+
+      size_ = std::exchange(v.size_, 0);
+      capacity_ = std::exchange(v.capacity_, 0);
+      data_ = std::exchange(v.data_, nullptr);
+    }
+
+    return *this;
+  }
+
+  vector &operator=(const vector &rhs) {
+    if (this != &rhs) {
+      delete[] data_;
+
+      if (rhs.size_ > 0) {
+        data_ = new value_type[rhs.size_];
+        if (data_ != nullptr) {
+          std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+        }
+      } else {
+        data_ = nullptr;
+      }
+      size_ = rhs.size_;
+      capacity_ = rhs.size_;
+    }
+    return *this;
+  }
+
+  // Element Access
+ public:
+  reference at(size_type pos) {
+    if (pos >= size_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[pos];
+  }
+
+  const_reference at(size_type pos) const {
+    if (pos >= size_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[pos];
+  }
+
+  reference operator[](size_type pos) { return data_[pos]; }
+
+  const_reference operator[](size_type pos) const {
+    if (pos >= size_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[pos];
+  }
+
+  reference front() {
+    if (size_ == 0) {
+      throw std::out_of_range("Vector is empty");
+    }
+    return data_[0];
+  }
+
+  const_reference front() const {
+    if (size_ == 0) {
+      throw std::out_of_range("Vector is empty");
+    }
+    return data_[0];
+  }
+
+  reference back() {
+    if (size_ == 0) {
+      throw std::out_of_range("Vector is empty");
+    }
+    return data_[size_ - 1];
+  }
+
+  const_reference back() const {
+    if (size_ == 0) {
+      throw std::out_of_range("Vector is empty");
+    }
+    return data_[size_ - 1];
+  }
+
+  iterator data() noexcept { return data_; }
+
+  const_iterator data() const noexcept { return data_; }
+
+  // Vector Iterators
+ public:
+  iterator begin() noexcept { return data_; }
+
+  const_iterator begin() const noexcept { return data_; }
+
+  const_iterator cbegin() const noexcept { return begin(); }
+
+  iterator end() noexcept { return data_ + size_; }
+
+  const_iterator end() const noexcept { return data_ + size_; }
+
+  const_iterator cend() const noexcept { return end(); }
+
+  // Vector Capacity
+ public:
+  bool empty() const noexcept { return size_ == 0; }
+
+  size_type size() const noexcept { return size_; }
+
+  size_type max_size() const noexcept {
+    return std::numeric_limits<size_type>::max();
+  }
+
+  void reserve(size_type new_capacity) {
+    if (new_capacity > capacity_) {
+      T *new_data = new T[new_capacity];
+      std::copy(data_, data_ + size_, new_data);
+      delete[] data_;
+      data_ = new_data;
+      capacity_ = new_capacity;
+    }
+  }
+
+  size_type capacity() const noexcept { return capacity_; }
+
+  void shrink_to_fit() {
+    if (size_ < capacity_) {
+      T *new_data = new T[size_];
+      std::copy(data_, data_ + size_, new_data);
+      delete[] data_;
+      data_ = new_data;
+      capacity_ = size_;
+    }
+  }
+
+  void clear() noexcept {
+    if (data_ != nullptr) {
+      delete[] data_;
+      data_ = nullptr;
+      size_ = 0;
+      capacity_ = 0;
+    }
+  }
+
+  iterator insert(iterator pos, const_reference value) {
+    size_type index = static_cast<size_type>(pos - begin());
+
+    if (index > size_) {
+      throw std::out_of_range("Out of range");
+    }
+
+    if (size_ == capacity_) {
+      reserve((capacity_ == 0) ? 1 : 2 * capacity_);
+    }
+
+    value_type tmp = value;
+
+    for (size_t i = size_; i > index; --i) {
+      *(data_ + i) = *(data_ + i - 1);
+    }
+
+    *(data_ + index) = tmp;
+    ++size_;
+
+    return begin() + index;
+  }
+
+  void erase(iterator pos) {
+    size_type index = pos - data_;
+    if (index < size_) {
+      std::copy(data_ + index + 1, data_ + size_, data_ + index);
+      --size_;
+    } else {
+      throw std::out_of_range("Out of range");
+    }
+  }
+
+  void push_back(const_reference value) {
+    if (size_ == capacity_) {
+      reserve((capacity_ == 0) ? 1 : 2 * capacity_);
+    }
+
+    data_[size_] = value;
+    ++size_;
+  }
+
+  void push_back(value_type &&value) {
+    if (size_ == capacity_) {
+      reserve((capacity_ == 0) ? 1 : 2 * capacity_);
+    }
+
+    data_[size_] = std::move(value);
+    ++size_;
+  }
+
+  void pop_back() {
+    if (size_ > 0) {
+      --size_;
+    }
+  }
+
+  void swap(vector &other) noexcept {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+  }
+
+  template <typename... Args>
+  iterator insert_many(const_iterator pos, Args &&...args) {
+    size_type index = pos - data_;
+
+    size_type num_elements = sizeof...(args);
+
+    if (size_ + num_elements > capacity_) {
+      reserve(std::max(2 * capacity_, size_ + num_elements));
+    }
+
+    std::copy_backward(data_ + index, data_ + size_,
+                       data_ + size_ + num_elements);
+
+    ((data_[index++] = std::forward<Args>(args)), ...);
+    size_ += num_elements;
+
+    return data_ + index - num_elements;
+  }
+
+  template <typename... Args>
+  void insert_many_back(Args &&...args) {
+    size_type num_elements = sizeof...(args);
+
+    if (size_ + num_elements > capacity_) {
+      reserve(std::max(2 * capacity_, size_ + num_elements));
+    }
+
+    ((data_[size_++] = std::forward<Args>(args)), ...);
+  }
 
  private:
-  void reserve_more_capacity(size_type size);
-
- public:
-  // default constructor
-  vector() : m_size(0U), m_capacity(0U), arr(nullptr) {}
-  // parametrized constructor for fixed size vector (explicit was used in order
-  // to avoid automatic type conversion)
-  explicit vector(size_type n)
-      : m_size(n), m_capacity(n), arr(n ? new T[n] : nullptr) {}
-  // initializer list constructor
-  vector(std::initializer_list<value_type> const &items);
-  // copy constructor with simplified syntax
-  vector(const vector &v)
-      : m_size(v.m_size), m_capacity(v.m_capacity), arr(v.arr){};
-  // move constructor with simplified syntax
-  vector(vector &&v) : m_size(v.m_size), m_capacity(v.m_capacity), arr(v.arr) {
-    v.arr = nullptr;
-    v.m_size = 0;
-  }
-
-  ~vector() { delete[] arr; }
-
-  // some method examples
-  // size getter
-  size_type size();
-  // element accessor
-  value_type at(size_type i);
-  // append new element
-  void push_back(value_type v);
+  size_type size_ = 0;
+  size_type capacity_ = 0;
+  iterator data_ = nullptr;
 };
-
-template <typename T>
-void vector<T>::reserve_more_capacity(size_t size) {
-  if (size > m_capacity) {
-    value_type *buff = new value_type[size];
-    for (size_t i = 0; i < m_size; ++i) buff[i] = std::move(arr[i]);
-    delete[] arr;
-    arr = buff;
-    m_capacity = size;
-  }
-}
-
-template <typename T>
-vector<T>::vector(std::initializer_list<value_type> const &items) {
-  arr = new value_type[items.size()];
-  int i = 0;
-  for (auto it = items.begin(); it != items.end(); it++) {
-    arr[i] = *it;
-    i++;
-  }
-  m_size = items.size();
-  m_capacity = items.size();
-}
-
-template <typename T>
-size_t vector<T>::size() {
-  return m_size;
-}
-
-template <typename T>
-T vector<T>::at(size_type i) {
-  return arr[i];
-}
-
-template <typename T>
-void vector<T>::push_back(T v) {
-  if (m_size == m_capacity) {
-    reserve_more_capacity(m_size * 2);
-  }
-  arr[m_size++] = v;
-}
 
 }  // namespace s21
 
---
+++ tests/array_test.cpp
@@ -0,0 +1,180 @@
+#include <array>
+
+#include "gtest/gtest.h"
+#include "s21_containers/s21_array.h"
+
+class ArrayTest : public ::testing::Test {
+ protected:
+  void SetUp() override {}
+  const s21::array<int, 5> arr1_{1, 2, 3, 4, 5};
+  s21::array<int, 5> arr0_{1, 2, 3, 4, 5};
+  s21::array<std::string, 1> arr2_{"world"};
+};
+
+TEST_F(ArrayTest, initializer_list_constructor) {
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(arr0_[i], i + 1);
+}
+
+TEST_F(ArrayTest, copy_constructor) {
+  s21::array<int, 5> arr1{arr0_};
+
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(arr0_[i], arr1[i]);
+}
+
+TEST_F(ArrayTest, move_constructor) {
+  s21::array<int, 5> arr1{std::move(arr0_)};
+
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(arr0_[i], arr1[i]);
+}
+
+TEST_F(ArrayTest, copy_assignment) {
+  s21::array<int, 5> arr1;
+  arr1 = arr0_;
+
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(arr0_[i], arr1[i]);
+}
+
+TEST_F(ArrayTest, move_assignment) {
+  s21::array<int, 5> arr1;
+  arr1 = std::move(arr0_);
+
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(arr0_[i], arr1[i]);
+}
+
+TEST_F(ArrayTest, cbegin_cend) {
+  auto it_begin = arr0_.cbegin();
+  for (std::size_t i = 0; i < arr0_.size(); ++i) ASSERT_EQ(*it_begin++, i + 1);
+
+  auto it_end = arr0_.cend();
+  ASSERT_EQ(it_end, arr0_.end());
+}
+
+TEST_F(ArrayTest, back) {
+  ASSERT_EQ(arr1_.back(), 5);
+  ASSERT_EQ(*(arr1_.end() - 1), 5);
+  ASSERT_EQ(arr0_.back(), 5);
+  ASSERT_EQ(*(arr0_.end() - 1), 5);
+}
+
+TEST_F(ArrayTest, front) {
+  ASSERT_EQ(arr1_.front(), 1);
+  ASSERT_EQ(*arr1_.begin(), 1);
+  ASSERT_EQ(arr0_.front(), 1);
+  ASSERT_EQ(*arr0_.begin(), 1);
+}
+
+TEST_F(ArrayTest, data) {
+  for (std::size_t i = 0; i < arr0_.size(); ++i)
+    ASSERT_EQ(*(arr0_.data() + i), arr0_[i]);
+}
+
+TEST_F(ArrayTest, iterator) {
+  int i = 1;
+  for (auto it = arr0_.begin(), end = arr0_.end(); it != end; ++it, ++i)
+    ASSERT_EQ(*it, i);
+
+  i = 1;
+  for (auto it = arr1_.begin(), end = arr1_.end(); it != end; ++it, ++i)
+    ASSERT_EQ(*it, i);
+}
+
+TEST_F(ArrayTest, at) {
+  for (std::size_t i = 0; i < arr0_.size(); ++i)
+    ASSERT_EQ(arr0_.at(3), arr0_[3]);
+}
+
+TEST_F(ArrayTest, at_exception) { EXPECT_ANY_THROW(arr0_.at(6)); }
+
+TEST_F(ArrayTest, size) { ASSERT_EQ(arr0_.size(), 5); }
+
+TEST_F(ArrayTest, max_size) { ASSERT_EQ(arr0_.max_size(), 5); }
+
+TEST_F(ArrayTest, empty) { ASSERT_FALSE(arr0_.empty()); }
+
+TEST(Array, test_all) {
+  s21::array<std::vector<int>, 10> a;
+  std::vector<int> v{1, 2, 3, 4, 5};
+  a.fill(v);
+
+  for (auto e : a) ASSERT_EQ(e, v);
+
+  ASSERT_ANY_THROW(a[11]);
+
+  ASSERT_ANY_THROW(a.at(11));
+  ASSERT_EQ(a.front(), a[0]);
+  ASSERT_EQ(a.front(), *a.data());
+  ASSERT_EQ(a.back(), a[9]);
+  ASSERT_FALSE(a.empty());
+  ASSERT_EQ(a.size(), 10);
+  ASSERT_EQ(a.max_size(), 10);
+
+  std::vector<int> w{0, 0, 0, 0, 0};
+  s21::array<std::vector<int>, 10> b;
+  b.fill(w);
+  a.swap(b);
+
+  for (auto e : a) ASSERT_EQ(e, w);
+
+  for (auto e : b) ASSERT_EQ(e, v);
+}
+
+TEST(Array, test_all_const) {
+  std::vector<int> v{1, 2, 3, 4, 5};
+  const s21::array<std::vector<int>, 10> a{v, v, v, v, v, v, v, v, v, v};
+  for (auto e : a) ASSERT_EQ(e, v);
+
+  ASSERT_ANY_THROW(a[11]);
+  ASSERT_ANY_THROW(a.at(11));
+  ASSERT_EQ(a.front(), a[0]);
+  ASSERT_EQ(a.front(), *a.data());
+  ASSERT_EQ(a.back(), a[9]);
+  ASSERT_FALSE(a.empty());
+  ASSERT_EQ(a.size(), 10);
+  ASSERT_EQ(a.max_size(), 10);
+
+  s21::array<std::vector<int>, 10> b;
+  b.fill(v);
+
+  for (int i = 0; i < 10; i++) ASSERT_EQ(a[i], b[i]);
+}
+
+TEST(Array, exception) {
+  ASSERT_ANY_THROW((s21::array<int, 5>{1, 2, 3, 4, 5, 6, 7}));
+  ASSERT_ANY_THROW((s21::array<int, 1>{1, 2, 3, 4, 5, 6, 7}));
+  ASSERT_ANY_THROW((s21::array<int, 2>{1, 2, 3, 4, 5, 6, 7}));
+  ASSERT_ANY_THROW((s21::array<int, 3>{1, 2, 3, 4, 5, 6, 7}));
+  ASSERT_NO_THROW((s21::array<int, 7>{1, 2, 3, 4, 5, 6, 7}));
+}
+
+TEST_F(ArrayTest, swap) {
+  s21::array<int, 5> arr1{5, 5, 5, 5, 5};
+  std::array<int, 5> arr2{5, 5, 5, 5, 5};
+  std::array<int, 5> arr3{1, 2, 3, 4, 5};
+
+  arr2.swap(arr3);
+  arr0_.swap(arr1);
+
+  for (s21::array<int, 5>::size_type i = 0; i < arr0_.size(); ++i)
+    ASSERT_EQ(arr0_[i], arr3[i]);
+}
+
+TEST_F(ArrayTest, swap2) {
+  s21::array<std::string, 1> arr1{"space"};
+  std::array<std::string, 1> arr2{"peace"};
+  std::array<std::string, 1> arr3{"space"};
+
+  arr2_.swap(arr1);
+  arr2.swap(arr3);
+
+  for (std::size_t i = 0; i < arr2_.size(); ++i) ASSERT_EQ(arr2_[i], arr2[i]);
+}
+
+TEST_F(ArrayTest, Fill) {
+  s21::array<int, 3> arr{};
+
+  arr.fill(42);
+
+  for (const auto& element : arr) {
+    EXPECT_EQ(element, 42);
+  }
+}
