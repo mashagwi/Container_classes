ba17f6836c966b56c78d3c69fc8d5f79cbe70a89
Anton Grevtsov <grevtsov.apn@gmail.com>
2024-01-21 16:31:49 +0700

    test: update tree and set tests

--- tests/rbtree_test.cc
+++ tests/rbtree_test.cc
@@ -1,29 +1,34 @@
 #include <algorithm>
+#include <iostream>
 #include <stdexcept>
 #include <string>
+#include <tuple>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 #include "gtest/gtest.h"
 #include "s21_containers/detail/s21_rbtree.h"
 
+// explicit template class instantiation for correct code coverage analysis
+template class s21::search_tree<int>;
+template class s21::search_tree<int, std::string>;
+
 TEST(SearchTreeTest, OneTemplateParameterInitializerList) {
   const s21::search_tree<int> t = {1, 2, 3};
   EXPECT_EQ(t.size(), 3);
-  EXPECT_EQ(t.empty(), false);
 }
 
 TEST(SearchTreeTest, TwoTemplateParametersInitializerList) {
   const s21::search_tree<int, std::string> t = {
       {1, "one"}, {2, "two"}, {3, "three"}};
   EXPECT_EQ(t.size(), 3);
-  EXPECT_EQ(t.empty(), false);
 }
 
 TEST(SearchTreeTest, CopyConstructor) {
   const s21::search_tree<int> t1 = {4, 7, 12, 15, 3, 5, 14, 18, 16, 17};
 
-  const s21::search_tree<int> t2(t1);
+  const s21::search_tree<int> t2(t1);  // NOLINT
   ASSERT_EQ(t2.size(), t1.size());
 
   auto iter1 = t1.begin();
@@ -48,15 +53,16 @@ TEST(SearchTreeTest, CopyAssignment) {
 }
 
 TEST(SearchTreeTest, MoveAssignment) {
-  const s21::search_tree<int> t1 = {4, 7, 12, 15, 3, 5, 14, 18, 16, 17};
+  s21::search_tree<int> t1 = {4, 7, 12, 15, 3, 5, 14, 18, 16, 17};  // NOLINT
+  s21::search_tree<int> ref(t1);
   s21::search_tree<int> t2;
 
-  t2 = t1;
-  ASSERT_EQ(t2.size(), t1.size());
+  t2 = std::move(t1);
+  ASSERT_EQ(t2.size(), ref.size());
 
-  auto iter1 = t1.begin();
+  auto iter1 = ref.begin();
   auto iter2 = t2.begin();
-  for (; iter1 != t1.end(); ++iter1, ++iter2) {
+  for (; iter1 != ref.end(); ++iter1, ++iter2) {
     ASSERT_EQ(*iter2, *iter1);
   }
 }
@@ -67,7 +73,7 @@ TEST(SearchTreeTest, IsEmptyInitially) {
   EXPECT_EQ(t.empty(), true);
 }
 
-TEST(SearchTreeTest, ProperOrder) {
+TEST(SearchTreeTest, ProperOrderDemonstration) {
   auto il = {4, 7, 12, 15, 3, 5, 14, 18, 16, 17};  // NOLINT
   std::vector<int> vec = il;
   sort(vec.begin(), vec.end());
@@ -80,6 +86,7 @@ TEST(SearchTreeTest, ProperOrder) {
     ASSERT_EQ(*iter, vec[i]);
   }
 
+  // NOLINTBEGIN
   tree.erase(3);
   tree.erase(12);
   tree.erase(12);
@@ -87,6 +94,7 @@ TEST(SearchTreeTest, ProperOrder) {
   tree.erase(18);
   tree.erase(15);
   tree.erase(16);
+  // NOLINTEND
 
   tree.clear();
   EXPECT_EQ(tree.size(), 0);
@@ -104,25 +112,40 @@ TEST(SearchTreeTest, IteratorUnderlyingConstness) {
 }
 
 TEST(SearchTreeTest, IteratorComparisons) {
-  const s21::search_tree<int> t = {1, 2, 3};
+  s21::search_tree<int> t = {1, 2, 3};  // NOLINT
   EXPECT_TRUE(t.begin() == t.cbegin());
   EXPECT_TRUE(t.begin() != t.cend());
 }
 
-TEST(SearchTreeTest, IteratorIteration) {
-  const s21::search_tree<int> t = {1, 2, 3};
+TEST(SearchTreeTest, IteratorIteration) {  // NOLINT
+  s21::search_tree<int> t = {1, 2, 3};
 
   auto iter = t.begin();
   ASSERT_EQ(*iter, 1);
   iter++;
   ASSERT_EQ(*iter, 2);
+  iter--;
+  ASSERT_EQ(*iter, 1);
+  ++iter;
   ++iter;
   ASSERT_EQ(*iter, 3);
   ++iter;
   ASSERT_EQ(iter, t.end());
+
+  auto riter = t.rbegin();
+  ASSERT_EQ(*riter, 3);
+  riter++;
+  ASSERT_EQ(*riter, 2);
+  --riter;
+  ++riter;
+  ASSERT_EQ(*riter, 2);
+  ++riter;
+  ASSERT_EQ(*riter, 1);
+  ++riter;
+  ASSERT_EQ(riter, t.rend());
 }
 
-TEST(SearchTreeTest, AtMethod) {
+TEST(SearchTreeTest, AtMethod) {  // NOLINT
   s21::search_tree<int, std::string> t = {{1, "one"}, {2, "two"}, {3, "three"}};
   EXPECT_EQ(t.at(1), "one");
   t.at(1) = "not one";
@@ -134,6 +157,7 @@ TEST(SearchTreeTest, AtMethod) {
 
 TEST(SearchTreeTest, BracketsOperator) {
   s21::search_tree<int, std::string> t = {{1, "one"}, {2, "two"}, {3, "three"}};
+  // NOLINTBEGIN
   EXPECT_EQ(t[1], "one");
   EXPECT_EQ(t[2], "two");
   EXPECT_EQ(t[3], "three");
@@ -141,4 +165,101 @@ TEST(SearchTreeTest, BracketsOperator) {
   EXPECT_EQ(t[5], "");
   t[5] = "five";
   EXPECT_EQ(t[5], "five");
+  // NOLINTEND
+}
+
+TEST(SearchTreeTest, InsertOrAssignMethod) {
+  s21::search_tree<int, std::string> t = {{1, "one"}, {2, "two"}, {3, "three"}};
+
+  auto [pos, res] = t.insert_or_assign(4, std::string("four"));
+  EXPECT_EQ(res, true);
+  EXPECT_EQ((*pos).second, "four");
+
+  std::tie(pos, res) = t.insert_or_assign(4, std::string("five"));
+  EXPECT_EQ(res, false);
+  EXPECT_EQ(t[4], "five");
+}
+
+TEST(SearchTreeTest, InsertIfNe2params) {
+  s21::search_tree<int, std::string> t = {{1, "one"}, {2, "two"}, {3, "three"}};
+
+  auto [pos, res] = t.insert_if_ne(4, std::string("four"));
+  EXPECT_EQ(res, true);
+  EXPECT_EQ((*pos).second, "four");
+
+  std::tie(pos, res) = t.insert_if_ne(4, std::string("five"));
+  EXPECT_EQ(res, false);
+  EXPECT_EQ(t[4], "four");
+}
+
+TEST(SearchTreeTest, BoundMethods) {  // NOLINT
+  s21::search_tree<int, std::string> t = {
+      {1, "one"}, {2, "two"}, {2, "two"}, {2, "two"}, {3, "three"}};
+
+  ASSERT_EQ(t.count(2), 3);
+
+  auto posl = t.lower_bound(2);
+  using value_type = s21::search_tree<int, std::string>::value_type;
+  ASSERT_EQ(*posl, value_type(2, "two"));
+  ASSERT_EQ((*posl).first, 2);
+  ASSERT_EQ((*posl).second, "two");
+
+  auto posu = t.upper_bound(2);
+  ASSERT_EQ(*posu, value_type(3, "three"));
+
+  auto [eql, equ] = t.equal_range(2);
+  EXPECT_EQ(posl, eql);
+  EXPECT_EQ(posu, equ);
+
+  posu--;
+  ASSERT_EQ(*posu, value_type(2, "two"));
+}
+
+TEST(SearchTreeTest, PrintMethod) {
+  const s21::search_tree<int, std::string> t = {
+      {1, "one"}, {2, "two"}, {3, "three"}};
+  std::clog << t;
+  std::clog << t.max_size() << '\n';
+  s21::search_tree<int> t2 = {5, 1, 6, 1234, 65, 1, 54, 0};  // NOLINT
+  std::clog << t2;
+  std::clog << t2.max_size() << '\n';
+}
+
+TEST(SearchTreeTest, MergeMethod) {
+  s21::search_tree<int> t = {1, 2, 3};   // NOLINT
+  s21::search_tree<int> t2 = {3, 4, 5};  // NOLINT
+  t.merge(t2);
+  t.merge(t2);
+  EXPECT_EQ(t.size(), 6);
+  EXPECT_EQ(t2.size(), 0);
+  s21::search_tree<int> t3;
+  t.merge(t3);
+  EXPECT_EQ(t.size(), 6);
+  EXPECT_EQ(t3.size(), 0);
+}
+
+int rand_i(unsigned i) {
+  // NOLINTBEGIN
+  i = (i << 13) ^ i;
+  return ((i * (i * i * 15731 + 789221) + 1376312589) & 0x7fffffff);
+  // NOLINTEND
+}
+TEST(SearchTreeTest, LargeOrderingTest) {
+  std::vector<int> v;
+  s21::search_tree<int> s;
+  for (int i = 0; i < 1000; i++) {  // NOLINT
+    const int rand = rand_i(i);
+    v.push_back(rand);
+    s.insert(rand);
+  }
+  sort(v.begin(), v.end());
+
+  int i = 0;
+  for (const auto& elem : s) {
+    std::cerr << "FUCK" << '\n';
+    std::cerr << i;
+    std::cerr << "FUCK" << '\n';
+    ASSERT_EQ(elem, v[i]);
+    ++i;
+  }
 }
--- tests/set_test.cc
+++ tests/set_test.cc
@@ -1,34 +1,145 @@
 
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <ostream>
+#include <tuple>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
 #include "gtest/gtest.h"
 #include "s21_containers/s21_set.h"
 
+// explicit template class instantiation for correct code coverage analysis
 template class s21::set<int>;
 
-TEST(SetTest, InsertMethod) {
-  s21::set<int> s = {1, 2, 3};
+// NOLINTBEGIN
+class SetTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    auto il_ = {4, 7, 12, 15, 3, 5, 14, 18, 16, 17, -1};  // NOLINT
+    iv_ = il_;
+    sort(iv_.begin(), iv_.end());
+    is_ = il_;
+  }
 
-  auto res_success = s.insert(4);
-  EXPECT_EQ(res_success.second, true);
-  EXPECT_EQ(*res_success.first, 4);
+  std::vector<int> iv_;
+  s21::set<int> is_;
+  int i_ = 0;
+};
+// NOLINTEND
 
-  auto res_failure = s.insert(4);
-  EXPECT_EQ(res_failure.second, false);
+TEST_F(SetTest, MutableIteration) {
+  for (auto iter = is_.begin(); iter != is_.end(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = is_.begin();  // NOLINT
+  EXPECT_FALSE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
+}
+TEST_F(SetTest, ReverseMutableIteration) {
+  std::reverse(iv_.begin(), iv_.end());
+  for (auto iter = is_.rbegin(); iter != is_.rend(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = is_.rbegin();  // NOLINT
+  EXPECT_FALSE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
 }
 
-TEST(SetTest, EraseMethod) {
-  s21::set<int> s = {4, 5, 6};
-
-  s.erase(4);
-  s.erase(5);
-  s.erase(6);
-  EXPECT_EQ(s.size(), 0);
+TEST_F(SetTest, NonMutableIteration) {
+  for (auto iter = is_.cbegin(); iter != is_.cend(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = is_.cbegin();  // NOLINT
+  EXPECT_TRUE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
+}
+TEST_F(SetTest, ReverseNonMutableIteration) {
+  std::reverse(iv_.begin(), iv_.end());
+  for (auto iter = is_.crbegin(); iter != is_.crend(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = is_.crbegin();  // NOLINT
+  EXPECT_TRUE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
 }
 
-TEST(SetTest, MergeMethod) {
+TEST_F(SetTest, NonMutableIterationConst) {
+  const s21::set<int> cis = is_;
+  for (auto iter = cis.begin(); iter != cis.end(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = cis.begin();  // NOLINT
+  EXPECT_TRUE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
+}
+TEST_F(SetTest, ReverseNonMutableIterationConst) {
+  std::reverse(iv_.begin(), iv_.end());
+  const s21::set<int> cis = is_;
+  for (auto iter = cis.rbegin(); iter != cis.rend(); ++iter, ++i_) {
+    ASSERT_EQ(*iter, iv_[i_]);
+  }
+  auto iter = cis.rbegin();  // NOLINT
+  EXPECT_TRUE(std::is_const_v<std::remove_reference_t<decltype(*iter)>>);
+}
+
+TEST_F(SetTest, FindMethod) {
+  const s21::set<int> cis = {1, 2, 3};
+  auto pos = cis.find(1);
+  EXPECT_EQ(*pos, 1);
+}
+
+TEST_F(SetTest, UniquenessProperty) {
+  s21::set<int> is = {1, 1, 2, 3, 4};
+  ASSERT_EQ(is.size(), 4);
+  is.insert(3);
+  ASSERT_EQ(is.size(), 4);
+  is.erase(1);
+  ASSERT_EQ(is.size(), 3);
+
+  const s21::set<int> is2 = {1, 1, 1};
+  ASSERT_EQ(is2.size(), 1);
+}
+
+TEST_F(SetTest, InsertMethod) {  // NOLINT
+  const bool is_exist = is_.contains(4);
+
+  auto [pos, res] = is_.insert(4);
+  if (is_exist)
+    EXPECT_EQ(res, false);
+  else
+    EXPECT_EQ(res, true);
+  EXPECT_EQ(*pos, 4);
+
+  std::tie(pos, res) = is_.insert(4);
+  EXPECT_EQ(res, false);
+  EXPECT_EQ(*pos, 4);
+
+  const int imax = std::numeric_limits<int>::max();
+  std::tie(pos, res) = is_.insert(imax);
+  EXPECT_EQ(res, true);
+  EXPECT_EQ(*pos, imax);
+}
+
+TEST_F(SetTest, EraseMethod) {
+  is_.insert(4);
+  const auto bsz = is_.size();
+  is_.erase(4);
+  EXPECT_EQ(is_.size(), bsz - 1);
+
+  is_.insert(4);
+  EXPECT_EQ(is_.size(), bsz);
+  auto pos = is_.find(4);
+  is_.erase(pos);
+  EXPECT_EQ(is_.size(), bsz - 1);
+}
+
+TEST_F(SetTest, InsertManyMethod) {
+  s21::set<int> is = {1, 2, 3};
+  is.insert_many(1, 2, 3, 4);
+  EXPECT_EQ(is.size(), 4);
+}
+
+TEST_F(SetTest, MergeMethod) {
   s21::set<int> s1 = {1, 3};
-  s21::set<int> s2 = {-1, 3, 2, 5, 6};
+  s21::set<int> s2 = {-1, 3, 2, 5, 6};  // NOLINT
 
   s1.merge(s2);
   EXPECT_EQ(s1.size(), 6);
@@ -36,26 +147,73 @@ TEST(SetTest, MergeMethod) {
   EXPECT_TRUE(s2.contains(3));
 }
 
-TEST(SetTest, CopyAssignmentOperator) {
-  s21::set<int> s1;
-  const s21::set<int> s2 = {1, 2, 3};
-
-  s1 = s2;
-  EXPECT_EQ(s1.size(), s2.size());
+TEST_F(SetTest, CopyAssignmentOperator) {
+  s21::set<int> s;
+  s = is_;
+  EXPECT_EQ(s.size(), is_.size());
 }
 
-TEST(SetTest, MoveAssignmentOperator) {
-  s21::set<int> s1;
-  s21::set<int> s2 = {1, 2, 3};
-  auto s2_size = s2.size();
-
-  s1 = std::move(s2);
-  EXPECT_EQ(s1.size(), s2_size);
+TEST_F(SetTest, SelfCopyAssignmentOperator) {
+  s21::set<int> s(is_);
+  s = s;
+  ASSERT_EQ(s.size(), is_.size());
+  for (const auto &elem : s) {
+    ASSERT_EQ(elem, iv_[i_]);
+    i_++;
+  }
 }
 
-TEST(SetTest, insertManyMethod) {
-  s21::set<int> s = {1, 2, 3};
+TEST_F(SetTest, MoveAssignmentOperator) {
+  auto s2_psize = is_.size();
+  s21::set<int> s;
+  s = std::move(is_);
+  EXPECT_EQ(s.size(), s2_psize);
+  EXPECT_TRUE(is_.empty());
+}
 
-  s.insert_many(4, 5, 6);
-  EXPECT_EQ(s.size(), 6);
+TEST_F(SetTest, SelfMoveAssignmentOperator) {
+  s21::set<int> s(is_);
+  s21::set<int> *p = &s;
+  s = std::move(*p);
+  ASSERT_EQ(s.size(), is_.size());
+  for (const auto &elem : s) {
+    ASSERT_EQ(elem, iv_[i_]);
+    i_++;
+  }
+}
+
+TEST_F(SetTest, MoveConstructor) {
+  auto s2_psize = is_.size();
+  const s21::set<int> s(std::move(is_));
+  EXPECT_EQ(s.size(), s2_psize);
+  EXPECT_TRUE(is_.empty());
+}
+
+TEST_F(SetTest, SwapMethod) {
+  s21::set<int> s;
+  EXPECT_TRUE(s.empty());
+  EXPECT_FALSE(is_.empty());
+  s.swap(is_);
+  EXPECT_FALSE(s.empty());
+  EXPECT_TRUE(is_.empty());
+}
+
+TEST_F(SetTest, EmptyMethod) {
+  EXPECT_FALSE(is_.empty());
+  is_.clear();
+  EXPECT_TRUE(is_.empty());
+  EXPECT_EQ(is_.size(), 0);
+}
+
+TEST_F(SetTest, ClearMethod) {
+  EXPECT_FALSE(is_.empty());
+  is_.clear();
+  EXPECT_TRUE(is_.empty());
+}
+
+TEST_F(SetTest, MaxSizeMethod) { std::clog << is_.max_size() << '\n'; }
+
+TEST_F(SetTest, EmptyInitializerListConstructor) {
+  const s21::set<int> s({});
+  EXPECT_TRUE(s.empty());
 }
